875ac3d8efda1a1eea27860c9509c4c85dd67a2e
leoleoyuuki <rm550373@fiap.com.br>
2023-09-04 17:12:51 -0300

    componentes Menu e Rodape added

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+
+import { Link } from "react-router-dom";
+
+export default function Menu(){
+
+    return(
+        <>
+            <nav className="menu">
+                <Link to='/'>Home</Link>
+                <span>|</span>
+                <Link to='/produtos'>Produtos</Link>
+            </nav>
+        </>
+    )
+
+}
